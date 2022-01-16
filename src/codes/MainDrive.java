52b9c731abd6c1467138218877dcecc2b76ab89d
허윤우 <hyoon516@naver.com>
2022-01-16 14:27:52 +0900

    6번 숫자 입력받기
    - 저장할 배열도 생성해둠

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,10 +1,31 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 		
 //		로또 시뮬레이터 제작
+//		1) 내 번호 6개 입력
+//		내 숫자 6개 저장 배열
+		int[] myNumberArr = new int[6];
+		
+//		컴퓨터 자원 낭비를 막기 위해, Scanner는 for문 밖에서 생성.
+		Scanner myScanner = new Scanner(System.in);
+		
+		for (int i = 0; i < myNumberArr.length; i++) {
+			
+//			내 번호를 자리에 입력받아 넣자. => Scanner 필요.
+			
+//			몇번째 숫자? 안내문.
+			
+			System.out.println(i+1+"번째 숫자 입력 : ");
+			
+//			숫자 입력받기.
+			int inpputNum = myScanner.nextInt();
+			
+		}
 
 	}
 
