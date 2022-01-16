2575f0d4cfb418c7b29590a988e05c7bb8f6d746
허윤우 <hyoon516@naver.com>
2022-01-16 14:47:53 +0900

    입력받은 숫자 검사 1. 범위 검사
    - 1 ~45 사이인가? 질문 결과를 boolean
    변수로 저장

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -27,6 +27,14 @@ public class MainDrive {
 				
 //				숫자 입력받기.
 				int inpputNum = myScanner.nextInt();
+				
+//				1. 1~45의 범위인가?
+				
+				boolean isRangeOk = (1 <= inpputNum) && (inpputNum <= 45);
+				
+//				2. 이미 등록된 숫자가 아닌가? => 중복이 아닌가?
+				
+//				1,2를 모두 통과하면 => 내 번호 배열에 등록 + 다음 숫자로 이동.
 			}
 
 			
